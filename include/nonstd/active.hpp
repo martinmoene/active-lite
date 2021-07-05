7f6c1703202cce8042af427191b1643fc1cb157c
Martin Moene <martin.moene@gmail.com>
2021-07-05 16:48:49 +0200

    Correct folder name

--- include/nonstd/active.hpp
+++ include/nonstd/active.hpp
@@ -1,7 +1,7 @@
 //
 // Copyright (c) 2020-2021 Martin Moene
 //
-// https://github.com/martinmoene/boolean-lite
+// https://github.com/martinmoene/active-lite
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
